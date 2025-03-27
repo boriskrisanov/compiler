e8fbf68515173c424349885f6ae2dec38c1f6e7f
Boris Krisanov <boriskrisanov1@gmail.com>
2025-03-27 10:28:31 +0000

    Initial commit

---
+++ src/main.py
@@ -0,0 +1,5 @@
+from tokenizer import tokenize
+
+src = "let x = 1" + "\n"
+
+print(tokenize(src))
---
+++ src/tokenizer.py
@@ -0,0 +1,40 @@
+from dataclasses import dataclass
+from enum import Enum
+
+class TokenKind(Enum):
+    KEYWORD_LET = 1
+    ASSIGNMENT_OPERATOR = 2
+    INTEGER_LITERAL = 3
+    IDENTIFIER = 4
+
+@dataclass
+class Token:
+    kind: TokenKind
+    value: any = None
+
+def tokenize(string: str) -> list[Token]:
+    tokens: list[Token] = []
+    buffer = ""
+
+    for c in string:
+        if c in [" ", "=", "\n"]:
+            # Keywords and literals
+            if buffer == "let":
+                tokens.append(Token(TokenKind.KEYWORD_LET))
+                buffer = ""
+            elif buffer != "":
+                if buffer.isnumeric():
+                    tokens.append(Token(TokenKind.INTEGER_LITERAL, int(buffer)))
+                else:
+                    tokens.append(Token(TokenKind.IDENTIFIER, buffer))
+                buffer = ""
+
+        if c != " ":
+            buffer += c
+
+        if buffer == "=":
+            tokens.append(Token(TokenKind.ASSIGNMENT_OPERATOR))
+            buffer = ""
+        
+
+    return tokens
